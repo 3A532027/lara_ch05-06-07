bc484be01f84794bb75fbb77962b2e70531242b5
3A532027 <s3a532027@student.nuct.edu.tw>
2019-01-20 02:12:00 +0800

    lara_ch05-06

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/','HomeController@index');
+Route::group(['namespace'=>'Cool'], function () {
+    Route::get('cool','TestController@index');
+});
+
+Route::pattern('student_no','s[0-9]{10}');
+Route::group(['prefix'=>'student'], function () {
+    Route::get('{student_no}',['as'=>'student','uses'=>'StudentController@getStudentData'
+    ]);
+
+    Route::get('{student_no}/score/{subject?}',['as'=>'student.score','uses'=>'StudentController@getStudentScore'
+    ])->where(['subject'=>'(chinese|english|math)']);
+});
+Route::get('/board', 'BoardController@getIndex');
+
