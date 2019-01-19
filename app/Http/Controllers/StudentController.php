00bc0f032ed6a981ee9ba33ec1e572a55fe42a89
3A532027 <s3a532027@student.nuct.edu.tw>
2019-01-20 02:54:05 +0800

    修正

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Student;
+use View;
 
 class StudentController extends Controller
 {
     public function getStudentData($student_no){
-        return view::make('score', ['student_no' => $student_no, 'subject' => null
+        return view('score', ['student_no' => $student_no, 'subject' => null
         ]);
     }
 
     public function getStudentScore($student_no,$subject = null)
     {
-        return view::make('score', ['student_no' => $student_no, 'subject' => $subject
+        return view('score', ['student_no' => $student_no, 'subject' => $subject
         ]);
     }
 
